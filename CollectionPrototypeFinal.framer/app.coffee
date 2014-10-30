9802c0fb859fd9d68ea4949b0d4b21c5cf60e962
Brian Armstrong <brianxarmstrong@gmail.com>
2014-10-29 18:52:15 -0700

    AddCanopyPrototype

---
+++ CollectionPrototypeFinal.framer/app.coffee
@@ -0,0 +1,140 @@
+# This imports all the layers for "CollectionPrototype" into collectionprototypeLayers
+s = Framer.Importer.load "imported/CollectionPrototype"
+
+
+
+Framer.Defaults.Animation = {
+	curve: 'cubic-bezier(0.4, 0, 0.2, 1)'
+	time: .2
+}
+
+curve =
+	spring: 'spring(400,30,0)'
+
+color =
+	canopy_green: "#23DEBF"
+	scrim: "rgba(0,0,0,.03)"
+
+animation = 
+	add: null
+	remove: null
+
+class CollectionRow extends Layer
+	constructor: (options) ->
+		super options
+
+		@inCollection = true
+
+		@width = Screen.width
+		@height = 190
+		@y = 720
+		@backgroundColor = null
+
+		@scrim = new Layer
+			width: Screen.width
+			height: @height
+			superLayer: @
+			backgroundColor: color.scrim
+		@scrim.bringToFront()
+		@scrim.opacity = 0
+
+		@on Events.Click, =>
+			if @inCollection
+				@removeProduct()
+			else 
+				@addProduct()
+				
+	stopAnimations: () =>
+		s.OtherProducts.animateStop()
+		s.ProductImageSmall.animateStop()
+		@scrim.animateStop()
+		
+	addProduct: () =>
+		print "Add to collection"
+		@inCollection = true
+		@stopAnimations()
+		
+		@productImageAnimation = s.ProductImageSmall.animate
+			properties: 
+				scale: 1
+				opacity: 1
+# 				x: 36
+			curve: curve.spring
+			
+		@scrim.animate
+			properties: 
+				opacity: 0
+				
+		s.OtherProducts.animate
+			properties: 
+				x: 152
+			curve: curve.spring
+			
+	removeProduct: () =>
+		print "Remove from collection"
+		@inCollection = false
+		@stopAnimations()
+		
+		@productImageAnimation = s.ProductImageSmall.animate
+			properties: 
+				scale: 0
+				opacity: 0
+# 				x: 0
+			curve: curve.spring
+				
+		@scrim.animate
+			properties: 
+				opacity: 1
+				
+		s.OtherProducts.animate
+			properties: 
+				x: 32
+			curve: curve.spring
+	
+
+activeRow = new CollectionRow
+
+class OutlinedButton extends Layer
+	constructor: (options) ->
+		super options
+		
+		@backgroundColor = null
+		@style = 
+			borderRadius: "8px"
+			border: "2px solid #23DEBF"
+
+# 		@touchButton = new Layer
+# 			height: options.height
+# 			width: options.width
+# 			superLayer: @
+# 			backgroundColor: color.canopy_green
+# 			opacity: 0
+# 		@touchButton.style =
+# 			border: "2px solid #23DEBF"
+		
+		@buttonText = new Layer
+			color: color.canopy_green
+			width: @width
+			superLayer: @
+			backgroundColor: null
+		@buttonText.html = "Create new collection"
+		@buttonText.style =
+			textAlign: "center"
+			fontFamily: "Europa"
+			lineHeight: @height - 6 + "px"
+		
+		@on Events.TouchStart, =>
+			@backgroundColor = color.canopy_green
+			@buttonText.color = "white"
+		
+		@on Events.TouchEnd, =>
+			@backgroundColor = null
+			@buttonText.color = color.canopy_green
+
+createCollectionButton = new OutlinedButton
+		width: 390
+		height: 70
+		y: 614
+createCollectionButton.centerX()
+
+s.RowScrim2.bringToFront()
---
+++ CollectionPrototypeFinal.framer/imported/CollectionPrototype/layers.json.js
@@ -0,0 +1,176 @@
+window.__imported__ = window.__imported__ || {};
+window.__imported__["CollectionPrototype/layers.json.js"] = [
+  {
+    "maskFrame" : null,
+    "id" : "F897200F-93F0-498B-9417-F1A0A40441B1",
+    "visible" : true,
+    "children" : [
+      {
+        "maskFrame" : null,
+        "id" : "E843ED68-C840-4102-950C-E1A0DA698550",
+        "visible" : true,
+        "children" : [
+
+        ],
+        "image" : {
+          "path" : "images\/RowScrim2-E843ED68-C840-4102-950C-E1A0DA698550.png",
+          "frame" : {
+            "y" : 911,
+            "x" : 0,
+            "width" : 640,
+            "height" : 207
+          }
+        },
+        "imageType" : "png",
+        "layerFrame" : {
+          "y" : 911,
+          "x" : 0,
+          "width" : 640,
+          "height" : 207
+        },
+        "name" : "RowScrim2"
+      },
+      {
+        "maskFrame" : null,
+        "id" : "1C037C99-B5B2-4F4D-A386-D04ABE9F97D0",
+        "visible" : true,
+        "children" : [
+
+        ],
+        "image" : {
+          "path" : "images\/ProductImageSmall-1C037C99-B5B2-4F4D-A386-D04ABE9F97D0.png",
+          "frame" : {
+            "y" : 788,
+            "x" : 36,
+            "width" : 82,
+            "height" : 95
+          }
+        },
+        "imageType" : "png",
+        "layerFrame" : {
+          "y" : 788,
+          "x" : 36,
+          "width" : 82,
+          "height" : 95
+        },
+        "name" : "ProductImageSmall"
+      },
+      {
+        "maskFrame" : null,
+        "id" : "D73B7AA1-6EF2-46E2-BE03-FE625C752182",
+        "visible" : true,
+        "children" : [
+          {
+            "maskFrame" : null,
+            "id" : "9A3019A5-5EF7-4767-9E3F-35F0153C524D",
+            "visible" : true,
+            "children" : [
+
+            ],
+            "image" : {
+              "path" : "images\/OtherProducts-9A3019A5-5EF7-4767-9E3F-35F0153C524D.png",
+              "frame" : {
+                "y" : 782,
+                "x" : 152,
+                "width" : 478,
+                "height" : 111
+              }
+            },
+            "imageType" : "png",
+            "layerFrame" : {
+              "y" : 782,
+              "x" : 152,
+              "width" : 478,
+              "height" : 111
+            },
+            "name" : "OtherProducts"
+          },
+          {
+            "maskFrame" : null,
+            "id" : "42B63491-1AB4-4353-84C9-5B04EBE2B78F",
+            "visible" : true,
+            "children" : [
+
+            ],
+            "image" : {
+              "path" : "images\/Box-42B63491-1AB4-4353-84C9-5B04EBE2B78F.png",
+              "frame" : {
+                "y" : 720,
+                "x" : 0,
+                "width" : 640,
+                "height" : 191
+              }
+            },
+            "imageType" : "png",
+            "layerFrame" : {
+              "y" : 720,
+              "x" : 0,
+              "width" : 640,
+              "height" : 191
+            },
+            "name" : "Box"
+          }
+        ],
+        "image" : {
+          "path" : "images\/CollectionRow-D73B7AA1-6EF2-46E2-BE03-FE625C752182.png",
+          "frame" : {
+            "y" : 720,
+            "x" : 0,
+            "width" : 640,
+            "height" : 191
+          }
+        },
+        "imageType" : "png",
+        "layerFrame" : {
+          "y" : 720,
+          "x" : 0,
+          "width" : 640,
+          "height" : 191
+        },
+        "name" : "CollectionRow"
+      },
+      {
+        "maskFrame" : null,
+        "id" : "3F119939-D533-4918-8B66-D46A0139614C",
+        "visible" : true,
+        "children" : [
+
+        ],
+        "image" : {
+          "path" : "images\/CollectionRow2-3F119939-D533-4918-8B66-D46A0139614C.png",
+          "frame" : {
+            "y" : 911,
+            "x" : 0,
+            "width" : 640,
+            "height" : 207
+          }
+        },
+        "imageType" : "png",
+        "layerFrame" : {
+          "y" : 911,
+          "x" : 0,
+          "width" : 640,
+          "height" : 207
+        },
+        "name" : "CollectionRow2"
+      }
+    ],
+    "image" : {
+      "path" : "images\/NewButton&Indicator-F897200F-93F0-498B-9417-F1A0A40441B1.png",
+      "frame" : {
+        "y" : 0,
+        "x" : 0,
+        "width" : 640,
+        "height" : 1136
+      }
+    },
+    "imageType" : "png",
+    "layerFrame" : {
+      "y" : 0,
+      "x" : 0,
+      "width" : 640,
+      "height" : 1136
+    },
+    "name" : "NewButton&Indicator"
+  }
+]
